d90d0c020d625ba6a8787fd8dbc1efa3210b8947
codemononoke <priyanshup891@gmail.com>
2024-05-04 21:37:00 +0530

    navbar created

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,8 +1,10 @@
 import React, { PropsWithChildren } from "react";
+import Navbar from "./_components/navbar";
 
 const MarketingLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className=" h-full bg-slate-100">
+      <Navbar />
       <main className=" pt-40 pb-20 bg-slate-100">{children}</main>
     </div>
   );
