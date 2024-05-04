a007e941f8150491648aa9943909b271d8423582
codemononoke <priyanshup891@gmail.com>
2024-05-04 22:49:10 +0530

    org navbar created and implemented

---
+++ app/(platform)/(dashboard)/layout.tsx
@@ -0,0 +1,13 @@
+import React, { PropsWithChildren } from "react";
+import Navbar from "./_components/navbar";
+
+const DashboardLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <div className=" h-full">
+      <Navbar />
+      {children}
+    </div>
+  );
+};
+
+export default DashboardLayout;
