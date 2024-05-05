1a397d2c00590488e8d231c023e874a32d5cadea
codemononoke <priyanshup891@gmail.com>
2024-05-05 14:09:50 +0530

    allowed hosts configured

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const OrganizationIdPage = () => {
-  return <div>OrganizationIdPage</div>;
+  async function create(formData: FormData) {
+    "use server";
+
+    console.log("I am triggered!");
+  }
+  return (
+    <div>
+      <form action={create}>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          placeholder="Enter a board title"
+          className=" border border-black p-1"
+          required
+        />
+      </form>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
