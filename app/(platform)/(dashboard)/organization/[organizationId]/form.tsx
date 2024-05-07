dd632ecd4dedbe772ba87ec10c95a4b03f806204
codemononoke <priyanshup891@gmail.com>
2024-05-07 09:17:47 +0530

    custom input and error implemented

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -4,6 +4,7 @@ import { useFormState } from "react-dom";
 import { Button } from "@/components/ui/button";
 import { createdBoard } from "@/actions/create-board";
 import { useAction } from "@/hooks/use-action";
+import FormInput from "@/components/form/form-input";
 
 const Form = () => {
   const { execute, fieldErrors } = useAction(createdBoard, {
@@ -23,24 +24,8 @@ const Form = () => {
   return (
     <form action={onSubmit}>
       <div className=" flex flex-col space-y-2">
-        <input
-          type="text"
-          id="title"
-          name="title"
-          placeholder="Enter a board title"
-          className=" border border-black p-1"
-          required
-        />
+        <FormInput label="Board Title" id="title" errors={fieldErrors} />
       </div>
-      {fieldErrors?.title ? (
-        <div>
-          {fieldErrors.title.map((error: string) => (
-            <p key={error} className=" text-rose-500">
-              {error}
-            </p>
-          ))}
-        </div>
-      ) : null}
       <Button type="submit">Submit</Button>
     </form>
   );
