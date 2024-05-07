260a62ac3488b694767ceeabd87782a151db0aff
codemononoke <priyanshup891@gmail.com>
2024-05-07 15:24:06 +0530

    board navbar created

--- app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { notFound, redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib/db";
+import BoardNavbar from "./_components/board-navbar";
 
 export async function generateMetadata({
   params,
@@ -49,6 +50,8 @@ const BoardIdLayout = async ({
       style={{ backgroundImage: `url(${board.imageFullUrl})` }}
       className=" relative h-full bg-no-repeat bg-cover bg-center"
     >
+      <BoardNavbar data={board} />
+      <div aria-hidden className=" absolute inset-0 bg-black/10" />
       <main className=" relative pt-28 h-full">{children}</main>
     </div>
   );
