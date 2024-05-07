36f8a5563cb569c7937f87947281f1f0253b6bad
codemononoke <priyanshup891@gmail.com>
2024-05-07 12:27:35 +0530

    dynamic title added

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,5 +1,14 @@
 import React, { PropsWithChildren } from "react";
 import OrgControl from "./_components/org-control";
+import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs/server";
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+  return {
+    title: startCase(orgSlug || "Organization"),
+  };
+}
 
 const organizationIdLayout = ({ children }: PropsWithChildren) => {
   return (
