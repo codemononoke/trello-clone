eb2832a5025eb09a7a0a14b67d25a82121a76a35
codemononoke <priyanshup891@gmail.com>
2024-05-11 11:19:47 +0530

    list header created

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,6 +1,8 @@
 "use client";
+import { useState, useEffect } from "react";
 import type { ListWithCards } from "@/types";
 import ListForm from "./list-form";
+import ListItem from "./list-item";
 
 type ListContainerProps = {
   data: ListWithCards[];
@@ -8,8 +10,17 @@ type ListContainerProps = {
 };
 
 const ListContainer = ({ data, boardId }: ListContainerProps) => {
+  const [orderedData, setOrderedData] = useState(data);
+
+  useEffect(() => {
+    setOrderedData(data);
+  }, [data]);
+
   return (
-    <ol>
+    <ol className=" flex gap-x-3 h-full">
+      {orderedData.map((list, i) => {
+        return <ListItem key={list.id} index={i} data={list} />;
+      })}
       <ListForm />
       <div aria-hidden className=" flex shrink-0 w-1" />
     </ol>
