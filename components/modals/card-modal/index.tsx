535679a9fc56805ff389d732effae883451cad12
codemononoke <priyanshup891@gmail.com>
2024-05-13 12:30:50 +0530

    card modal created

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -2,14 +2,26 @@
 import React from "react";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { useCardModal } from "@/hooks/use-card-modal";
+import { useQuery } from "@tanstack/react-query";
+import Header from "./header";
+import { CardWithList } from "@/types";
+import { fetcher } from "@/lib/fetcher";
 
 const CardModal = () => {
   const id = useCardModal((state) => state.id);
   const isOpen = useCardModal((state) => state.isOpen);
   const onClose = useCardModal((state) => state.onClose);
+
+  const { data: cardData } = useQuery<CardWithList>({
+    queryKey: ["card", id],
+    queryFn: () => fetcher(`/api/cards/${id}`),
+  });
+
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent>I am a modal</DialogContent>
+      <DialogContent>
+        {cardData ? <Header data={cardData} /> : <Header.Skeleton />}
+      </DialogContent>
     </Dialog>
   );
 };
