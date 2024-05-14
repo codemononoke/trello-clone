a50f1a0ee435b820dff254db402dac96fc25ceec
codemononoke <priyanshup891@gmail.com>
2024-05-14 11:02:29 +0530

    activity logs done

--- components/modals/card-modal/activity.tsx
+++ components/modals/card-modal/activity.tsx
@@ -2,13 +2,27 @@
 import React from "react";
 import { AuditLog } from "@prisma/client";
 import { Skeleton } from "@/components/ui/skeleton";
+import { ActivityIcon } from "lucide-react";
+import ActivityItem from "@/components/activity-item";
 
 type ActivityProps = {
   data: AuditLog[];
 };
 
 const Activity = ({ data }: ActivityProps) => {
-  return <div>Activity</div>;
+  return (
+    <div className=" flex items-start gap-x-3 w-full">
+      <ActivityIcon className=" h-5 w-5 mt-0.5 text-neutral-700" />
+      <div className=" w-full">
+        <p className=" font-semibold text-neutral-700 mb-2">Activity</p>
+        <ol className=" mt-2 space-y-4">
+          {data.map((item) => (
+            <ActivityItem key={item.id} data={item} />
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
 };
 
 Activity.Skeleton = function ActivitySkeleton() {
