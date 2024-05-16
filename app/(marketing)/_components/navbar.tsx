632a34e66d2510a92055a4eae6f1cef8708633e6
codemononoke <priyanshup891@gmail.com>
2024-05-16 15:58:27 +0530

    github link added

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Link from "next/link";
 import Logo from "@/components/logo";
 import { buttonVariants } from "@/components/ui/button";
+import { Github } from "lucide-react";
 
 const Navbar = () => {
   return (
     <div className=" fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
       <div className=" md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
         <Logo />
-        <div className=" space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+        <div className=" space-x-4  md:w-auto flex items-center justify-between w-full">
           <Link
             href="/sign-in"
             className={buttonVariants({ size: "sm", variant: "outline" })}
@@ -17,6 +18,14 @@ const Navbar = () => {
           </Link>
           <Link href="/sign-up" className={buttonVariants({ size: "sm" })}>
             Get Taskify for free
+          </Link>
+          <Link
+            href="https://github.com/codemononoke/trello-clone"
+            target="_blank"
+            rel="noreferrer noopener"
+            className={buttonVariants({ size: "sm" })}
+          >
+            <Github className=" h-4 w-4" />
           </Link>
         </div>
       </div>
