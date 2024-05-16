8295c3b59f82b156e390be28bfa59d4380ebeed1
codemononoke <priyanshup891@gmail.com>
2024-05-16 12:41:45 +0530

    board count decrement on delete board action

--- actions/delete-board/index.ts
+++ actions/delete-board/index.ts
@@ -8,6 +8,7 @@ import { createSafeAction } from "@/lib/create-safe-action";
 import { redirect } from "next/navigation";
 import { ACTION, ENTITY_TYPE } from "@prisma/client";
 import { createAuditLog } from "@/lib/create-audit-log";
+import { decreaseAvailableCount } from "@/lib/org-limit";
 
 const handler = async (date: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -29,6 +30,8 @@ const handler = async (date: InputType): Promise<ReturnType> => {
         orgId,
       },
     });
+
+    await decreaseAvailableCount();
 
     await createAuditLog({
       entityId: board.id,
