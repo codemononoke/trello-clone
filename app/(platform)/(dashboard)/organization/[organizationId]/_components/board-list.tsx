dc1e2c38e5da421d8d2f7bde629524828edab7fc
codemononoke <priyanshup891@gmail.com>
2024-05-16 12:19:13 +0530

    orgLimit counts calculated

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -7,6 +7,8 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 import { Skeleton } from "@/components/ui/skeleton";
 import { db } from "@/lib/db";
+import { getAvailableCount } from "@/lib/org-limit";
+import { MAX_FREE_BOARDS } from "@/constants/boards";
 
 const BoardList = async () => {
   const { orgId } = auth();
@@ -21,6 +23,9 @@ const BoardList = async () => {
       createdAt: "desc",
     },
   });
+
+  const availableCount = await getAvailableCount();
+
   return (
     <div className=" space-y-4">
       <div className=" flex items-center font-semibold text-lg text-neutral-700">
@@ -48,7 +53,9 @@ const BoardList = async () => {
             className=" aspect-video relative h-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
           >
             <p className=" text-sm">Create new board</p>
-            <span className=" text-xs">5 remaining</span>
+            <span className=" text-xs">
+              {`${MAX_FREE_BOARDS - availableCount} remaining`}
+            </span>
             <Hint
               sideOffset={40}
               description={`Free workspace can hove upto 5 open boards. For unlimited boards, please upgrade this workspace.`}
